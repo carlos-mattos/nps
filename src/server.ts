4fe0659a8de37ab4fc7a21521d730d54ce538105
carlos-mattos <ceamattos.jf@gmail.com>
2021-04-13 20:38:26 -0300

    aula02

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
+import "reflect-metadata";
+import "./database/index.ts";
 import express from "express";
+import router from "./routes";
 
 const app = express();
 app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => {
   console.log("Server running in 3333");
